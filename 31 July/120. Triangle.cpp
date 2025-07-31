1f397eb61c3b38e28a1d4aabb96b7f6ca363144c
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-07-31 13:37:37 +0530

    completed today's DSA-work

--- 31 July/120. Triangle.cpp
+++ 31 July/120. Triangle.cpp
@@ -25,6 +25,7 @@ public:
         for (int i = n - 2; i >= 0; i--)
             for (int j = 0; j <= i; j++)
                 dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+                
         return dp[0];
     }
 };
