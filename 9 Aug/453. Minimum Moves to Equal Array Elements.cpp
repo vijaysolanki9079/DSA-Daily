e374d99b4b4bf4e1d1ad7c026c4fb7ebcedf83bd
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-09 18:54:49 +0530

    streak done

--- 9 Aug/453. Minimum Moves to Equal Array Elements.cpp
+++ 9 Aug/453. Minimum Moves to Equal Array Elements.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int moves = 0;
-        int maxEle = *max_element(nums.begin(), nums.end());
+        /* ----- 1 (wring answer) -------*/
+        // int moves = 0;
+        // int maxEle = *max_element(nums.begin(), nums.end());
 
-        for(int i=0 ; i<nums.size() ; ++i){
-            if(nums[i] == maxEle){
-                continue;
-            } else {
-                moves += abs(nums[i] - maxEle);
-            }
-        } 
+        // for(int i=0 ; i<nums.size() ; ++i){
+        //     if(nums[i] == maxEle){
+        //         continue;
+        //     } else {
+        //         moves += abs(nums[i] - maxEle);
+        //     }
+        // } 
 
-        return moves;
+        // return moves;
 
+
+        /* ------ 2 ------*/
+        long long sum = 0;
+        int minEle = INT_MAX;
+
+        for (int num : nums) {
+            sum += num;
+            minEle = min(minEle, num);
+        }
+        return sum - (long long)minEle * nums.size(); 
     }
 };
