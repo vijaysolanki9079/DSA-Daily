8676a056929fe777a5190f56c31db7ff4383ca12
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-09 16:32:58 +0530

    streak done

--- 9 Aug/43. Multiply Strings.cpp
+++ 9 Aug/43. Multiply Strings.cpp
@@ -7,8 +7,33 @@ public:
         // return result;
 
         /* ------ 2 -------------*/
-        long long a = stoll(num1), b = stoll(num2);
-        long long product = a * b;
-        return to_string(product);
+        // long long a = stoll(num1), b = stoll(num2);
+        // long long product = a * b;
+        // return to_string(product);
+
+        /* ----- 3 -------*/
+        if (num1 == "0" || num2 == "0") return "0";
+
+        int n = num1.size();
+        int m = num2.size();
+        vector<int> result(n + m, 0);
+
+        // Reverse iteration for multipli..
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        // Convert vector -> string
+        string ans;
+        for (int num : result) {
+            if (!(ans.empty() && num == 0)) ans.push_back(num + '0');
+        }
+        return ans.empty() ? "0" : ans;
     }
 };
