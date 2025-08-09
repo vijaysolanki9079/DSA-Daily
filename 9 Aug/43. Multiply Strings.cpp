41f801fa0cd08c0ba9a1537c18b3f02927c1089d
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-09 16:32:19 +0530

    streak done

--- 9 Aug/43. Multiply Strings.cpp
+++ 9 Aug/43. Multiply Strings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         /*  -------1 -------*/
-        int a = stoi(num1), b = stoi(num2);
-        string result = to_string(a*b);
-        return result;
+        // int a = stoi(num1), b = stoi(num2);
+        // string result = to_string(a*b);
+        // return result;
 
         /* ------ 2 -------------*/
-        // long long a = stoll(num1), b = stoll(num2);
-        // long long product = a * b;
-        // return to_string(product);
+        long long a = stoll(num1), b = stoll(num2);
+        long long product = a * b;
+        return to_string(product);
     }
 };
