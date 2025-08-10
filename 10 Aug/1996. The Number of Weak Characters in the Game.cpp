4a63efa26423ea3764a6e4ce81a79675a58a900f
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-10 15:04:58 +0530

    code done

--- 10 Aug/1996. The Number of Weak Characters in the Game.cpp
+++ 10 Aug/1996. The Number of Weak Characters in the Game.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        int n = properties.size();
-        int cnt = 0;
+        /*  -------- 1 --------*/
+        // int n = properties.size();
+        // int cnt = 0;
 
-        for(auto it : properties){
-            int first = it[0];
-            int second = it[1];
+        // for(auto it : properties){
+        //     int first = it[0];
+        //     int second = it[1];
 
-            bool check = false;
-            for(auto k : properties){
-                if(first < k[0] && second < k[1]){
-                    check = true;
-                }
+        //     bool check = false;
+        //     for(auto k : properties){
+        //         if(first < k[0] && second < k[1]){
+        //             check = true;
+        //         }
+        //     }
+        //     if(check == true) {
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        /* ---- 2 ------*/
+
+        // sorting it  
+        sort(properties.begin(), properties.end(), [](auto &a, auto &b){
+            if (a[0] == b[0]) return a[1] < b[1]; 
+            return a[0] > b[0]; 
+        });
+
+        // finding the weak characters
+        int maxDefense = INT_MIN;
+        int weakCount = 0;
+
+        for (auto &p : properties) {
+            if (p[1] < maxDefense) {
+                weakCount++;
             }
-            if(check == true) {
-                cnt++;
-            }
+            maxDefense = max(maxDefense, p[1]);
         }
-        return cnt;
+
+        return weakCount;
     }
 };
