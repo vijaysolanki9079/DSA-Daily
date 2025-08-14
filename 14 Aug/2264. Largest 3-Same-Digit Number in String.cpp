d14450f9c2d9b62068ed93476ed9114e14b7f0e7
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-14 18:30:26 +0530

    streak done

--- 14 Aug/2264. Largest 3-Same-Digit Number in String.cpp
+++ 14 Aug/2264. Largest 3-Same-Digit Number in String.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
+    // string largestGoodInteger(string num) {
+    //     string ans = "";
+    //     for (int i = 0; i + 2 < num.size(); i++) {
+    //         if (num[i] == num[i+1] && num[i] == num[i+2]) {
+    //             string temp = num.substr(i, 3); 
+    //             if (temp > ans) ans = temp; 
+    //         }
+    //     }
+    //     return ans;
+    // }
     string largestGoodInteger(string num) {
-        string ans = "";
-        for (int i = 0; i + 2 < num.size(); i++) {
-            if (num[i] == num[i+1] && num[i] == num[i+2]) {
-                string temp = num.substr(i, 3); 
-                if (temp > ans) ans = temp; 
+        char best = 0;
+        int count = 1;
+        for (int i = 1; i < num.size(); i++) {
+            if (num[i] == num[i-1]) {
+                count++;
+                if (count >= 3) best = max(best, num[i]);
+            } else {
+                count = 1;
             }
         }
-        return ans;
+        return best ? string(3, best) : "";
     }
 };
