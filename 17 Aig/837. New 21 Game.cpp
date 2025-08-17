b05108235acae5912fed5e05f65fe592aa0916dc
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-17 12:03:50 +0530

    streak done

--- 17 Aig/837. New 21 Game.cpp
+++ 17 Aig/837. New 21 Game.cpp
@@ -16,25 +16,44 @@ public:
     // }
 
     /* ---- 2 -----*/
+    // double new21Game(int n, int k, int maxPts) {
+    //     if (k == 0 || n >= k + maxPts - 1) return 1.0;
+
+    //     vector<double> dp(n + 1, 0.0);
+    //     dp[0] = 1.0;
+    //     double windowSum = 1.0;  
+    //     double ans = 0.0;
+
+    //     for (int i = 1; i <= n; ++i) {
+    //         dp[i] = windowSum / maxPts;
+    //         if (i < k) {
+    //             windowSum += dp[i];               
+    //         } else {
+    //             ans += dp[i];                     
+    //         }
+    //         if (i - maxPts >= 0) {
+    //             windowSum -= dp[i - maxPts];      
+    //         }
+    //     }
+    //     return ans;
+    // }
+
     double new21Game(int n, int k, int maxPts) {
         if (k == 0 || n >= k + maxPts - 1) return 1.0;
 
-        vector<double> dp(n + 1, 0.0);
+        const int LIM = n + maxPts;             
+        vector<double> dp(LIM + 1, 0.0);
         dp[0] = 1.0;
-        double windowSum = 1.0;  
-        double ans = 0.0;
 
-        for (int i = 1; i <= n; ++i) {
-            dp[i] = windowSum / maxPts;
-            if (i < k) {
-                windowSum += dp[i];               
-            } else {
-                ans += dp[i];                     
-            }
-            if (i - maxPts >= 0) {
-                windowSum -= dp[i - maxPts];      
+        for (int i = 0; i < k; ++i) {
+            double add = dp[i] / maxPts;
+            for (int x = 1; x <= maxPts; ++x) {
+                dp[i + x] += add;
             }
         }
+
+        double ans = 0.0;
+        for (int s = k; s <= n; ++s) ans += dp[s];
         return ans;
     }
 };
