5cdc37c49d65ff770c7ec69af067bae55af09050
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-18 11:10:58 +0530

    code done

--- 18 Aug/343. Integer Break.cpp
+++ 18 Aug/343. Integer Break.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
+    // int integerBreak(int n) {
+    //     vector<int> dp(n+1, 0);
+
+    //     dp[1] = 0;  
+    //     for (int i = 2; i <= n; i++) {
+    //         for (int j = 1; j < i; j++) {
+
+    //             // case1: split into two
+    //             int noSplit = j * (i - j);
+
+    //             // case2: split further 
+    //             int split = j * dp[i - j];
+
+    //             dp[i] = max(dp[i], max(noSplit, split));
+    //         }
+    //     }
+
+    //     return dp[n];
+    // }
+
     int integerBreak(int n) {
-        vector<int> dp(n+1, 0);
+        if (n == 2) return 1;
+        if (n == 3) return 2;
 
-        dp[1] = 0;  
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-
-                // case1: split into two
-                int noSplit = j * (i - j);
-
-                // case2: split further 
-                int split = j * dp[i - j];
-
-                dp[i] = max(dp[i], max(noSplit, split));
-            }
+        int product = 1;
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
         }
-
-        return dp[n];
+        return product * n;
     }
 };
