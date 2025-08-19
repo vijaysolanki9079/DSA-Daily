448b97297b96c407a399079ba2bfa4852a786419
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-19 18:21:22 +0530

    code done

---
+++ 19 Aug/419. Battleships in a Board.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+Solution {
+public:
+    /* ----- 1 ------*/
+    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& vis){
+      if(r<0 ||c < 0 || r>=n || c>= m) return;
+      if(vis[r][c] || board[r][c] != 'X') return;
+      vis[r][c] = 1;
+
+      // Explore 4 neighbors
+        dfs(r+1, c, board);
+        dfs(r-1, c, board);
+        dfs(r, c+1, board);
+        dfs(r, c-1, board);
+    }
+
+    int countBattleships(vector<vector<char>>& board){
+      int n = board.size();
+      int m = board[0].size();
+      vector<vector<int>> vis(n, vector<int>(m, 0));
+       
+      int ships = 0;
+      for(int i=0 ; i<n ; ++i){
+        for(int i=0 ; i<n ; ++i){
+            if(!visited[i][j] && board[i][j] == 'X'){
+              ships++;
+              dfs(i,j, board, vis);
+            }
+        }
+      }
+
+      return ships;
+    }
+
+    
+
+};
+
+
