3c86f98f620f6e6a1ce478b14fce6ef314ea8a87
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-19 22:34:54 +0530

    code done

--- 19 Aug/463. Island Perimeter.cpp
+++ 19 Aug/463. Island Perimeter.cpp
@@ -1,30 +1,55 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
+    /*------  1 ----------*/
+    // int islandPerimeter(vector<vector<int>>& grid) {
+    //     int n = grid.size();
+    //     int m = grid[0].size();
+    //     int peri = 0;
+
+    //     for (int i = 0; i < n; ++i) {
+    //         for (int j = 0; j < m; ++j) {
+    //             if (grid[i][j] == 1) {
+    //                 peri += 4;
+
+    //                 // check up
+    //                 if (i > 0 && grid[i-1][j] == 1) peri--;
+    //                 // check down
+    //                 if (i < n-1 && grid[i+1][j] == 1) peri--;
+    //                 // check left
+    //                 if (j > 0 && grid[i][j-1] == 1) peri--;
+    //                 // check right
+    //                 if (j < m-1 && grid[i][j+1] == 1) peri--;
+    //             }
+    //         }
+    //     }
+    //     return peri;
+    // }
+
+    /* -------- 2 ------*/
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if (i < 0 || j < 0 || i >= n || j >= m) return 1;
+        if (grid[i][j] == 0) return 1;
+        if (grid[i][j] == -1) return 0;
+
+        grid[i][j] = -1;
+
+        return dfs(grid, i+1, j) + dfs(grid, i-1, j) +
+               dfs(grid, i, j+1) + dfs(grid, i, j-1);
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int peri = 0;
 
-        // Approach - check all the direction for each i.e. Element = 1
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (grid[i][j] == 1) {
-                    peri += 4;
-
-                    // check up
-                    if (i > 0 && grid[i-1][j] == 1) peri--;
-                    // check down
-                    if (i < n-1 && grid[i+1][j] == 1) peri--;
-                    // check left
-                    if (j > 0 && grid[i][j-1] == 1) peri--;
-                    // check right
-                    if (j < m-1 && grid[i][j+1] == 1) peri--;
+                    return dfs(grid, i, j);
                 }
             }
         }
-        return peri;
+        return 0;
     }
 };
