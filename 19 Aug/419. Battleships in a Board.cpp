568c4050d65ab626ad2131d9c3497b44c3ed1203
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-19 18:39:55 +0530

    code done

--- 19 Aug/419. Battleships in a Board.cpp
+++ 19 Aug/419. Battleships in a Board.cpp
@@ -40,38 +40,56 @@ public:
     //     return ships;
     // }
     /*------- 2--------*/
+    // int countBattleships(vector<vector<char>>& board) {
+    //     int n = board.size();
+    //     int m = board[0].size();
+    //     vector<vector<int>> vis(n, vector<int>(m, 0));
+
+    //     int ships = 0;
+    //     int dr[4] = {1, -1, 0, 0};
+    //     int dc[4] = {0, 0, 1, -1};
+
+    //     for (int i = 0; i < n; ++i) {
+    //         for (int j = 0; j < m; ++j) {
+    //             if (!vis[i][j] && board[i][j] == 'X') {
+    //                 ships++;
+    //                 queue<pair<int,int>> q;
+    //                 q.push({i,j});
+    //                 vis[i][j] = 1;
+
+    //                 while (!q.empty()) {
+    //                     auto [r,c] = q.front(); q.pop();
+    //                     for (int k=0; k<4; k++) {
+    //                         int nr = r + dr[k], nc = c + dc[k];
+    //                         if (nr>=0 && nc>=0 && nr<n && nc<m &&
+    //                             !vis[nr][nc] && board[nr][nc] == 'X') {
+    //                             vis[nr][nc] = 1;
+    //                             q.push({nr,nc});
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ships;
+    // }
+
+    /*------ 3 -------*/
     int countBattleships(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        int ships = 0;
-        int dr[4] = {1, -1, 0, 0};
-        int dc[4] = {0, 0, 1, -1};
+        int count = 0;
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                if (!vis[i][j] && board[i][j] == 'X') {
-                    ships++;
-                    queue<pair<int,int>> q;
-                    q.push({i,j});
-                    vis[i][j] = 1;
-
-                    while (!q.empty()) {
-                        auto [r,c] = q.front(); q.pop();
-                        for (int k=0; k<4; k++) {
-                            int nr = r + dr[k], nc = c + dc[k];
-                            if (nr>=0 && nc>=0 && nr<n && nc<m &&
-                                !vis[nr][nc] && board[nr][nc] == 'X') {
-                                vis[nr][nc] = 1;
-                                q.push({nr,nc});
-                            }
-                        }
-                    }
+                if (board[i][j] == 'X') {
+                    if (i > 0 && board[i-1][j] == 'X') continue; // connected vertically
+                    if (j > 0 && board[i][j-1] == 'X') continue; // connected horizontally
+                    count++;
                 }
             }
         }
-        return ships;
+        return count;
     }
 };
 
