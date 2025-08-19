099ae99a6501d13d48cf6f8e40394a5008ec9d69
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-19 15:47:34 +0530

    streak done

--- 19 Aug/2348. Number of Zero-Filled Subarrays.cpp
+++ 19 Aug/2348. Number of Zero-Filled Subarrays.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& nums) {
-        long long count = 0;
-        int n = nums.size();
-        int i = 0;
+    // long long zeroFilledSubarray(vector<int>& nums) {
+    //     long long count = 0;
+    //     int n = nums.size();
+    //     int i = 0;
 
-        while (i < n) {
-            if (nums[i] == 0) {
-                int j = i;
-                while (j < n && nums[j] == 0) {
-                    j++;
-                }
-                long long len = j - i; // length of zero block
-                count += (len * (len + 1LL)) / 2; // formula
-                i = j; // skip this block
+    //     while (i < n) {
+    //         if (nums[i] == 0) {
+    //             int j = i;
+    //             while (j < n && nums[j] == 0) {
+    //                 j++;
+    //             }
+    //             long long len = j - i; // length of zero block
+    //             count += (len * (len + 1LL)) / 2; // formula
+    //             i = j; // skip this block
+    //         } else {
+    //             i++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long cnt = 0;  
+        long long strk = 0; 
+
+        for (int num : nums) {
+            if (num == 0) {
+                strk++;            
+                cnt += strk;     
             } else {
-                i++;
+                strk = 0;          
             }
         }
-        return count;
+        return cnt;
     }
 };
