b48058117fb445801f69e9ca46023939e6973388
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-20 22:34:39 +0530

    streak done

--- 20 Aug/1277. Count Square Submatrices with All Ones.cpp
+++ 20 Aug/1277. Count Square Submatrices with All Ones.cpp
@@ -1,52 +1,102 @@
 class Solution {
 public:
     /*--------- 1 ---------- */
-    int count1s = 0;
-    int count2s = 0;
-    int count3s = 0;
+    // int count1s = 0;
+    // int count2s = 0;
+    // int count3s = 0;
 
-    void findThreeSideMatrix(int i, int j, vector<vector<int>>& matrix){
-        int n = matrix.size();
-        int m = matrix[0].size();
+    // void findThreeSideMatrix(int i, int j, vector<vector<int>>& matrix){
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
 
-        if(i+2 >= n || j+2 >= m) return; 
+    //     if(i+2 >= n || j+2 >= m) return; 
 
-        for(int x = i; x < i+3; x++) {
-            for(int y = j; y < j+3; y++) {
-                if(matrix[x][y] != 1) return;
+    //     for(int x = i; x < i+3; x++) {
+    //         for(int y = j; y < j+3; y++) {
+    //             if(matrix[x][y] != 1) return;
+    //         }
+    //     }
+    //     count3s++;
+    // }
+
+    // void findTwoSideMatrix(int i, int j, vector<vector<int>>& matrix){
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
+
+    //     if(i+1 >= n || j+1 >= m) return; 
+
+    //     if(matrix[i][j] == 1 && matrix[i][j+1] == 1 &&
+    //        matrix[i+1][j] == 1 && matrix[i+1][j+1] == 1) {
+    //         count2s++;
+    //         findThreeSideMatrix(i, j, matrix); 
+    //     }
+    // } 
+
+    // int countSquares(vector<vector<int>>& matrix) {
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
+
+    //     count1s = count2s = count3s = 0;
+
+    //     for(int i=0 ; i<n ; ++i){
+    //         for(int j=0 ; j<m ; ++j){
+    //             if(matrix[i][j] == 1) {
+    //                 count1s++;
+    //                 findTwoSideMatrix(i, j, matrix);
+    //             }
+    //         }
+    //     }
+        
+    //     return count1s + count2s + count3s; 
+    // }
+
+    /* ------- 2 ------- */
+    int fun(vector<vector<int>>& matrix, int side) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int count = 0;
+
+        // We can only start at (i,j) where submatrix of size 'side' fits
+        for (int i = 0; i + side - 1 < m; i++) {
+            for (int j = 0; j + side - 1 < n; j++) {
+                bool allOnes = true;
+
+                // Check this submatrix
+                for (int x = i; x < i + side; x++) {
+                    for (int y = j; y < j + side; y++) {
+                        if (matrix[x][y] == 0) {
+                            allOnes = false;
+                            break;
+                        }
+                    }
+                    if (!allOnes) break;
+                }
+
+                if (allOnes) count++;
             }
         }
-        count3s++;
+        return count;
     }
 
-    void findTwoSideMatrix(int i, int j, vector<vector<int>>& matrix){
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        if(i+1 >= n || j+1 >= m) return; 
-
-        if(matrix[i][j] == 1 && matrix[i][j+1] == 1 &&
-           matrix[i+1][j] == 1 && matrix[i+1][j+1] == 1) {
-            count2s++;
-            findThreeSideMatrix(i, j, matrix); 
-        }
-    } 
-
     int countSquares(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int squares = 0;
 
-        count1s = count2s = count3s = 0;
-
-        for(int i=0 ; i<n ; ++i){
-            for(int j=0 ; j<m ; ++j){
-                if(matrix[i][j] == 1) {
-                    count1s++;
-                    findTwoSideMatrix(i, j, matrix);
-                }
+        // Count 1x1 squares
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) squares++;
             }
         }
-        
-        return count1s + count2s + count3s; 
+
+        int mini = min(m, n);
+
+        // Count larger squares
+        for (int side = 2; side <= mini; side++) {
+            squares += fun(matrix, side);
+        }
+
+        return squares;
     }
 };
