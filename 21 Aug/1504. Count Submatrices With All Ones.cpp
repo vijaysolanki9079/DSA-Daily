5a577f192c8b278da6dca79a8d376e4cd3e81127
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-22 23:13:18 +0530

    streak done

--- 21 Aug/1504. Count Submatrices With All Ones.cpp
+++ 21 Aug/1504. Count Submatrices With All Ones.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
     /* ------- 1 Brute Force (slow) ------- */
+    // int numSubmat(vector<vector<int>>& mat) {
+    //     int m = mat.size(), n = mat[0].size();
+    //     int ans = 0;
+
+    //     for (int i1 = 0; i1 < m; i1++) {
+    //         for (int j1 = 0; j1 < n; j1++) {
+    //             for (int i2 = i1; i2 < m; i2++) {
+    //                 for (int j2 = j1; j2 < n; j2++) {
+    //                     bool allOnes = true;
+    //                     for (int x = i1; x <= i2 && allOnes; x++) {
+    //                         for (int y = j1; y <= j2; y++) {
+    //                             if (mat[x][y] == 0) {
+    //                                 allOnes = false;
+    //                                 break;
+    //                             }
+    //                         }
+    //                     }
+    //                     if (allOnes) ans++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    /* ------- 2 ------ */
     int numSubmat(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> height(m, vector<int>(n, 0));
+        
+        // Build height matrix
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                if (mat[i][j] == 1) {
+                    height[i][j] = (i == 0 ? 1 : height[i-1][j] + 1);
+                }
+            }
+        }
+        
         int ans = 0;
-
-        for (int i1 = 0; i1 < m; i1++) {
-            for (int j1 = 0; j1 < n; j1++) {
-                for (int i2 = i1; i2 < m; i2++) {
-                    for (int j2 = j1; j2 < n; j2++) {
-                        bool allOnes = true;
-                        for (int x = i1; x <= i2 && allOnes; x++) {
-                            for (int y = j1; y <= j2; y++) {
-                                if (mat[x][y] == 0) {
-                                    allOnes = false;
-                                    break;
-                                }
-                            }
-                        }
-                        if (allOnes) ans++;
-                    }
+        // Process each row as histogram
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int minHeight = height[i][j];
+                for (int k = j; k >= 0 && minHeight > 0; k--) {
+                    minHeight = min(minHeight, height[i][k]);
+                    ans += minHeight;
                 }
             }
         }
         return ans;
     }
-};
+}
