e532135adde8d7b6b25ebc865ee6dbf1fb377ddb
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-08-22 23:13:30 +0530

    streak done

--- 21 Aug/1504. Count Submatrices With All Ones.cpp
+++ 21 Aug/1504. Count Submatrices With All Ones.cpp
@@ -27,30 +27,63 @@ public:
     // }
 
     /* ------- 2 ------ */
+    // int numSubmat(vector<vector<int>>& mat) {
+    //     int m = mat.size(), n = mat[0].size();
+    //     vector<vector<int>> height(m, vector<int>(n, 0));
+        
+    //     // Build height matrix
+    //     for (int j = 0; j < n; j++) {
+    //         for (int i = 0; i < m; i++) {
+    //             if (mat[i][j] == 1) {
+    //                 height[i][j] = (i == 0 ? 1 : height[i-1][j] + 1);
+    //             }
+    //         }
+    //     }
+        
+    //     int ans = 0;
+    //     // Process each row as histogram
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             int minHeight = height[i][j];
+    //             for (int k = j; k >= 0 && minHeight > 0; k--) {
+    //                 minHeight = min(minHeight, height[i][k]);
+    //                 ans += minHeight;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    /* ----- 3 (mono-tonic stack) ------ */
     int numSubmat(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> height(m, vector<int>(n, 0));
-        
-        // Build height matrix
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                if (mat[i][j] == 1) {
-                    height[i][j] = (i == 0 ? 1 : height[i-1][j] + 1);
-                }
-            }
-        }
-        
+        vector<int> height(n, 0);
         int ans = 0;
-        // Process each row as histogram
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int minHeight = height[i][j];
-                for (int k = j; k >= 0 && minHeight > 0; k--) {
-                    minHeight = min(minHeight, height[i][k]);
-                    ans += minHeight;
+                if (mat[i][j] == 0) height[j] = 0;
+                else height[j]++;
+            }
+
+            stack<int> st;
+            vector<int> sum(n, 0);
+
+            for (int j = 0; j < n; j++) {
+                while (!st.empty() && height[st.top()] >= height[j])
+                    st.pop();
+
+                if (!st.empty()) {
+                    int prev = st.top();
+                    sum[j] = sum[prev] + height[j] * (j - prev);
+                } else {
+                    sum[j] = height[j] * (j + 1);
                 }
+
+                st.push(j);
+                ans += sum[j];
             }
         }
         return ans;
     }
-}
+};
