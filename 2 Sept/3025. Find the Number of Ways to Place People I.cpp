f23d07cd62da07bb366fdd8ced8729fcbfb6f7ee
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-09-02 16:36:32 +0530

    streak done

--- 2 Sept/3025. Find the Number of Ways to Place People I.cpp
+++ 2 Sept/3025. Find the Number of Ways to Place People I.cpp
@@ -1,3 +1,19 @@
+struct Fenwick {
+    vector<int> bit;
+    int n;
+    Fenwick(int n): n(n), bit(n+1, INT_MIN) {}
+    void update(int idx, int val) {
+        for (++idx; idx <= n; idx += idx & -idx)
+            bit[idx] = max(bit[idx], val);
+    }
+    int query(int idx) {
+        int res = INT_MIN;
+        for (++idx; idx > 0; idx -= idx & -idx)
+            res = max(res, bit[idx]);
+        return res;
+    }
+};
+
 class Solution {
 public:
     /* ------- 1 ------- */
@@ -33,6 +49,28 @@ public:
     // }
 
     /* --------- 2 -------------*/
+    // int numberOfPairs(vector<vector<int>>& points) {
+    //     sort(points.begin(), points.end(), [](auto &a, auto &b) {
+    //         if (a[0] != b[0]) return a[0] < b[0];
+    //         return a[1] > b[1];
+    //     });
+
+    //     int n = points.size();
+    //     int ans = 0;
+
+    //     for (int i = 0; i < n; i++) {
+    //         int maxY = INT_MIN;
+    //         for (int j = i + 1; j < n; j++) {
+    //             if (points[i][1] >= points[j][1] && points[j][1] > maxY) {
+    //                 ans++;
+    //                 maxY = points[j][1];
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    /* -------- 3 -------- */ 
     int numberOfPairs(vector<vector<int>>& points) {
         sort(points.begin(), points.end(), [](auto &a, auto &b) {
             if (a[0] != b[0]) return a[0] < b[0];
@@ -41,13 +79,17 @@ public:
 
         int n = points.size();
         int ans = 0;
+        int maxY = 51; // since y ≤ 50
+        Fenwick bit(maxY);
 
         for (int i = 0; i < n; i++) {
-            int maxY = INT_MIN;
-            for (int j = i + 1; j < n; j++) {
-                if (points[i][1] >= points[j][1] && points[j][1] > maxY) {
-                    ans++;
-                    maxY = points[j][1];
+            int maxSeen = INT_MIN;
+            for (int j = i+1; j < n; j++) {
+                if (points[i][1] >= points[j][1]) {
+                    if (points[j][1] > maxSeen) {
+                        ans++;
+                        maxSeen = points[j][1];
+                    }
                 }
             }
         }
