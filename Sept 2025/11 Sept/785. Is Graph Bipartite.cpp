82adfd090b3ddea55426152f5a52a83daea563be
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-09-12 23:01:13 +0530

    code done

---
+++ Sept 2025/11 Sept/785. Is Graph Bipartite.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    /* ------- 1 ---------
+    Time: O(V + E)
+    Space: O(V)               */ 
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1); // -1: uncolored
+
+        for (int i = 0; i < n; ++i) {
+            if (color[i] == -1) {
+                queue<int> q;
+                q.push(i);
+                color[i] = 0;
+
+                while (!q.empty()) {
+                    int node = q.front(); q.pop();
+                    for (int neighbor : graph[node]) {
+                        if (color[neighbor] == -1) {
+                            color[neighbor] = 1 - color[node];
+                            q.push(neighbor);
+                        } else if (color[neighbor] == color[node]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+  };
